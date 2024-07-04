8476605e08ee1ef94a1745585435678b3223f579
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-07-04 14:26:33 +0200

    implements Ball as actor for first MWEof game

--- src/pool/PoolGame.java
+++ src/pool/PoolGame.java
@@ -3,14 +3,15 @@ package pool;
 import ch.aplu.jgamegrid.GameGrid;
 import ch.aplu.jgamegrid.Location;
 
-import java.awt.*;
-
 public class PoolGame {
     public static void main(String[] args) {
         GameGrid gameGrid = new GameGrid(40, 22, 20, java.awt.Color.red, false);
         gameGrid.setBgImagePath("assets/pool_table.png");
         gameGrid.setTitle("Pool Game");
         gameGrid.doRun();
+
+        Ball ball = new Ball();
+        gameGrid.addActor(ball, new Location(11,11));
 
         gameGrid.show();
     }
