167b41b5b7de1420abd127d77c3032077780f3a2
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-07-04 15:27:27 +0200

    some comments for better readability

--- src/pool/BallPositions.java
+++ src/pool/BallPositions.java
@@ -6,10 +6,15 @@ public class BallPositions {
     public static final Location CUE_BALL_POSITION = new Location(200, 219);
 
     public static final Location [] TRIANGLE_POSITIONS = {
-            new Location(600, 219), // first row
-            new Location(620, 209), new Location(620, 229), // second row
-            new Location(640, 199), new Location(640, 219), new Location(640, 239), //third row
+            // first row
+            new Location(600, 219),
+            // second row
+            new Location(620, 209), new Location(620, 229),
+            //third row
+            new Location(640, 199), new Location(640, 219), new Location(640, 239),
+            // forth row
             new Location(660, 189), new Location(660, 209), new Location(660, 229), new Location(660, 249),
+            // fifth row
             new Location(680, 179), new Location(680, 199), new Location(680, 219), new Location(680, 239), new Location(680, 259)
     };
 }
