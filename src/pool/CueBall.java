3fe0ce4d9bfe3f3bd17979bdfc98b3a70187f130
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-07-15 19:16:07 +0200

    adds image path as a constant

--- src/pool/CueBall.java
+++ src/pool/CueBall.java
@@ -3,10 +3,11 @@ package pool;
 import ch.aplu.jgamegrid.Actor;
 
 public class CueBall extends Actor {
+    private static final String IMAGE_PATH = "assets/kugel_white.gif";
     private static final int BALL_SIZE = 20;
 
     public CueBall() {
-        super("assets/white_kugel.gif");
+        super(IMAGE_PATH);
     }
 
     public void act() {
