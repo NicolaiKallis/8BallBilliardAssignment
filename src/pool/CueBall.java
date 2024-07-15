d2b82ae830344d337be9f23ae289e35de0222530
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-07-15 18:14:34 +0200

    implements basic structure of separate CueBall class

---
+++ src/pool/CueBall.java
@@ -0,0 +1,15 @@
+package pool;
+
+import ch.aplu.jgamegrid.Actor;
+
+public class CueBall extends Actor {
+    private static final int BALL_SIZE = 20;
+
+    public CueBall() {
+        super("assets/white_kugel.gif");
+    }
+
+    public void act() {
+
+    }
+}
