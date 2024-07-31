ec51932536aa0e7534c003a3883ab8ad987d7809
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-07-31 23:58:21 +0200

    subclasses CueBall.java from Ball.java

--- src/pool/CueBall.java
+++ src/pool/CueBall.java
@@ -1,16 +1,14 @@
 package pool;
 
 import ch.aplu.jgamegrid.Actor;
+import ch.aplu.jgamegrid.Location;
+import ch.aplu.jgamegrid.GGVector;
 
-public class CueBall extends Actor {
+public class CueBall extends Ball {
     private static final String IMAGE_PATH = "assets/kugel_white.gif";
     private static final int BALL_SIZE = 20;
 
-    public CueBall() {
-        super(IMAGE_PATH);
-    }
-
-    public void act() {
-
+    public CueBall(int wallOffset) {
+        super(IMAGE_PATH, 0);
     }
 }
