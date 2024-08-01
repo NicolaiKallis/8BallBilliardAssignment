9a239f2e450964d8d680b4024580c94129fb4b8b
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-08-01 21:51:21 +0200

    adds label and panel for status display

--- src/pool/PoolGame.java
+++ src/pool/PoolGame.java
@@ -9,19 +9,23 @@ public class PoolGame{
     public static void main(String[] args) {
         JFrame gameFrame = GameFrame.createGameFrame();
         JLabel gameTimer = GameFrame.createGameTimerLabel();
+        JLabel gameState = GameFrame.createStatusLabel();
         JLabel ballCounter = GameFrame.createBallCounterLabel();
 
         JPanel southPanel = new JPanel(new BorderLayout());
-        JPanel timerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel timerPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JPanel statePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JPanel ballCounterPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        PoolTable poolTable = new PoolTable(gameTimer, ballCounter);
+        PoolTable poolTable = new PoolTable(gameTimer, ballCounter, gameState);
         poolTable.doRun();
 
         timerPanel.add(gameTimer);
+        statePanel.add(gameState);
         ballCounterPanel.add(ballCounter);
 
-        southPanel.add(timerPanel, BorderLayout.CENTER);
+        southPanel.add(timerPanel, BorderLayout.EAST);
+        southPanel.add(statePanel, BorderLayout.CENTER);
         southPanel.add(ballCounterPanel, BorderLayout.WEST);
 
         gameFrame.setLayout(new BorderLayout());
