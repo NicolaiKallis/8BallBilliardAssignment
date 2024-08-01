04d176bde53c51cfd7fb75a989978726a1c1cf8a
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-08-01 03:42:29 +0200

    adds pocket locations

--- src/pool/PoolTable.java
+++ src/pool/PoolTable.java
@@ -1,7 +1,9 @@
 package pool;
 
-import java.util.List;
+import java.util.ArrayList;
 
+import ch.aplu.jgamegrid.GGVector;
+import ch.aplu.jgamegrid.GGCircle;
 import ch.aplu.jgamegrid.GameGrid;
 import ch.aplu.jgamegrid.Location;
 
@@ -10,13 +12,40 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class PoolTable extends GameGrid implements MouseMotionListener, MouseListener {
+public class PoolTable extends GameGrid implements MouseMotionListener,
+        MouseListener {
 
-    private static final int TABLE_LENGTH       = 800;
-    private static final int TABLE_WIDTH        = 438;
-    public static final int TABLE_WALL_OFFSET  = 40;
+    private static final int TABLE_LENGTH               = 800;
+    private static final int TABLE_WIDTH                = 438;
+    public static final int TABLE_WALL_OFFSET           = 50;
 
-    private static final int CELL_SIZE          = 1;
+    private static final int CELL_SIZE                  = 1;
+
+    public static final double FRICTION_CLOTH_BALL      = 0.2;
+    private static final GGVector POCKET_TOP_LEFT       = new GGVector(25, 25);
+    private static final GGVector POCKET_TOP_MIDDLE     = new GGVector(400, 25);
+    private static final GGVector POCKT_TOP_RIGHT       = new GGVector(775, 25);
+    private static final GGVector POCKET_BOTTOM_LEFT    = new GGVector(25, 413);
+    private static final GGVector POCKET_BOTTOM_MIDDLE  = new GGVector(400, 413);
+    private static final GGVector POCKET_BOTTOM_RIGHT   = new GGVector(775,
+            413);
+
+    // assumed a ratio of 2:1 (pocket size: balls)
+    // assumption made from: https://www.dimensions.com/element/billiards-pool-table-pockets
+    // increased radius even further than 2:1 for UX
+    private static final int POCKET_RADIUS              = 50;
+
+    public static final ArrayList<GGCircle> pockets = new ArrayList();
+
+    static {
+            pockets.add(new GGCircle(POCKET_TOP_LEFT, POCKET_RADIUS));
+            pockets.add(new GGCircle(POCKET_TOP_MIDDLE, POCKET_RADIUS));
+            pockets.add(new GGCircle(POCKT_TOP_RIGHT, POCKET_RADIUS));
+            pockets.add(new GGCircle(POCKET_BOTTOM_LEFT, POCKET_RADIUS));
+            pockets.add(new GGCircle(POCKET_BOTTOM_MIDDLE, POCKET_RADIUS));
+            pockets.add(new GGCircle(POCKET_BOTTOM_RIGHT, POCKET_RADIUS));
+    }
+
 
     private static final String TABLE_ASSET = "assets/pool_table.png";
 
@@ -24,8 +53,8 @@ public class PoolTable extends GameGrid implements MouseMotionListener, MouseLis
     private CueBall cueBall;
 
     public PoolTable() {
-        super(TABLE_LENGTH, TABLE_WIDTH, CELL_SIZE, null, TABLE_ASSET, false);
-        setSimulationPeriod(10);
+        super(TABLE_LENGTH, TABLE_WIDTH, CELL_SIZE, null, TABLE_ASSET,false);
+        setSimulationPeriod(20);
         addMouseMotionListener(this);
         addMouseListener(this);
 
@@ -41,13 +70,13 @@ public class PoolTable extends GameGrid implements MouseMotionListener, MouseLis
     }
 
     public void setBallsToStartPositions(){
-        List<Integer> ballIndices = BallPositions.getRandomBallIndices();
-
-        for (int i=0; i < BallPositions.TRIANGLE_POSITIONS.length; i++) {
-            int BallIndex = ballIndices.get(i);
-            Ball ball = new Ball(Ball.ballAssets[BallIndex - 1], BallIndex);
-            addActor(ball, BallPositions.TRIANGLE_POSITIONS[i]);
-        }
+//        List<Integer> ballIndices = BallPositions.getRandomBallIndices();
+//
+//        for (int i=0; i < BallPositions.TRIANGLE_POSITIONS.length; i++) {
+//            int BallIndex = ballIndices.get(i);
+//            Ball ball = new Ball(Ball.ballAssets[BallIndex - 1], BallIndex);
+//            addActor(ball, BallPositions.TRIANGLE_POSITIONS[i]);
+//        }
 
         cueBall = new CueBall(TABLE_WALL_OFFSET);
         addActor(cueBall, BallPositions.CUE_BALL_POSITION);
@@ -62,6 +91,13 @@ public class PoolTable extends GameGrid implements MouseMotionListener, MouseLis
         addActor(cueStick, initialCueStickPosition);
     }
 
+
+
+
+
+
+
+
     @Override
     public void mouseMoved(MouseEvent e) {
         Point mousePos = e.getPoint();
@@ -70,7 +106,7 @@ public class PoolTable extends GameGrid implements MouseMotionListener, MouseLis
         Location cueBallLocation = cueBall.getLocation();
         Point cueBallPosition = new Point (cueBallLocation.x , cueBallLocation.y);
         double angle = Math.atan2(mouseLocation.getY() - cueBall.getY(), mouseLocation.getX() - cueBall.getX());
-        cueStick.updatePosition(Math.toDegrees(angle), cueBallPosition);
+        cueStick.updatePosition(Math.toDegrees(angle));
         this.repaint();
     }
 
@@ -83,7 +119,7 @@ public class PoolTable extends GameGrid implements MouseMotionListener, MouseLis
         Location cueBallLocation = cueBall.getLocation();
         Point cueBallPosition = new Point (cueBallLocation.x , cueBallLocation.y);
         double angle = Math.atan2(mouseLocation.getY() - cueBall.getY(), mouseLocation.getX() - cueBall.getX());
-        cueStick.pullBack(Math.toDegrees(angle), cueBallPosition);
+        cueStick.pullBackCue(Math.toDegrees(angle));
         this.repaint();
 
     }
@@ -96,12 +132,9 @@ public class PoolTable extends GameGrid implements MouseMotionListener, MouseLis
         Point mousePos = e.getPoint();
         Location mouseLocation = toLocationInGrid(mousePos);
 
-        Location cueBallLocation = cueBall.getLocation();
-        Point cueBallPosition = new Point (cueBallLocation.x , cueBallLocation.y);
         double angle = Math.atan2(mouseLocation.getY() - cueBall.getY(), mouseLocation.getX() - cueBall.getX());
 
-
-        cueStick.hitCueBall(angle, cueBallPosition, cueBall);
+        cueStick.hitCueBall(angle);
 
         this.repaint();
     }
@@ -122,6 +155,4 @@ public class PoolTable extends GameGrid implements MouseMotionListener, MouseLis
     @Override
     public void mouseExited(MouseEvent e) {
     }
-
-
 }
