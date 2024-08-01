5ddb0dbfefb80409bb7a5753d586a5dd46cd19db
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-08-01 23:35:53 +0200

    final commit

--- src/pool/CueStick.java
+++ src/pool/CueStick.java
@@ -1,85 +1,75 @@
 package pool;
 
 import ch.aplu.jgamegrid.Actor;
+import ch.aplu.jgamegrid.GGVector;
 import ch.aplu.jgamegrid.GameGrid;
 import ch.aplu.jgamegrid.Location;
-
-import java.awt.Point;
 
 import java.util.Objects;
 
 // Note: the arrow image was flipped 180° to implement the functionality of the cue stick more easily
 public class CueStick extends Actor {
-    private static final String IMAGE_PATH = "assets/arrow.png";
-    private static final double INITIAL_ANGLE = 0d;
-    public static final int DISTANCE_FROM_CUE_BALL = 20;
+    private static final String IMAGE_PATH              = "assets/arrow.png";
+    public static final int DISTANCE_FROM_CUE_BALL      = 20;
     private static final int MAX_DISTANCE_FROM_CUE_BALL = 90;
-    private CueBall cueBall; //--> TODO: Has to connected to the actual cue ball
-    private GameGrid gameGrid;
+    // assumed value for good UX
+    private static final float FORCE_CUESTICK_HIT       = 300f;
+
+    private boolean visible;
+    private boolean enabled;
+
+    private final CueBall cueBall;
 
     public CueStick(CueBall cueBall, GameGrid gameGrid) {
         super(true, IMAGE_PATH); // Initialize without an image
         Objects.requireNonNull(cueBall);
         this.cueBall = cueBall;
         this.gameGrid = gameGrid;
-        //updatePosition(INITIAL_ANGLE); // Initialize position
-
     }
 
     @Override
     public void act() {
-        Point mousePosition = gameGrid.getMousePosition();
-        System.out.println(mousePosition);
-        if (mousePosition != null) {
-            Location mouseLocation = gameGrid.toLocationInGrid(mousePosition);
-
-            double dx = mouseLocation.x - cueBall.getX();
-            double dy = mouseLocation.y - cueBall.getY();
-            double angleInDegrees = Math.toDegrees(Math.atan2(dy, dx));
-
-            //updatePosition(angleInDegrees);
+        if (!enabled) {
+            return;
         }
     }
 
-    // TODO: implement correctly: Cue Stick should sit in the same direction as mouse
-    // TODO: Naming convention should be improved
-    public void updatePosition(double angle, Point cueBallPosition) {
-        double correctedAngle = angle + 180;
+    private void updateCueStickPosition(double angle, int distance) {
         double radians = Math.toRadians(angle);
 
-        int x = (int) (cueBall.getX() + DISTANCE_FROM_CUE_BALL * Math.cos(radians));
-        int y = (int) (cueBall.getY() + DISTANCE_FROM_CUE_BALL * Math.sin(radians));
+        int x = (int) (cueBall.getX() + distance * Math.cos(radians));
+        int y = (int) (cueBall.getY() + distance * Math.sin(radians));
 
         setLocation(new Location(x,y));
-        //System.out.println(angle);
-        //System.out.println(correctedAngle);
-        setDirection(correctedAngle);
-        rotate(cueBallPosition, correctedAngle);
+        setDirection(angle+180);
     }
 
-    public void pullBack(double angle, Point cueBallPosition) {
-        double correctedAngle = angle + 180;
-        double radians = Math.toRadians(angle);
-
-        int x = (int) (cueBall.getX() + MAX_DISTANCE_FROM_CUE_BALL * Math.cos(radians));
-        int y = (int) (cueBall.getY() + MAX_DISTANCE_FROM_CUE_BALL * Math.sin(radians));
-
-        setLocation(new Location(x,y));
-        Point Goal = new Point(x,y);
-        //System.out.println(angle);
-        //System.out.println(correctedAngle);
-        setDirection(correctedAngle);
-        rotate(cueBallPosition, correctedAngle);
-        //updatePosition(correctedAngle, Goal);
+    public void updateCueStartPosition(double angle) {
+        updateCueStickPosition(angle, DISTANCE_FROM_CUE_BALL);
     }
 
-    public void hitCueBall(double angle, Point cueBallPosition) {
-        double raadians = Math.toRadians(angle);
-
-        int x = (int) cueBall.getX();
-        int y = (int) cueBall.getY();
-
-        setLocation(new Location(x,y));
+    public void pullBackCue(double angle) {
+        updateCueStickPosition(angle, MAX_DISTANCE_FROM_CUE_BALL);
     }
 
+    public void hitCueBall(double radians) {
+        if (!enabled) {
+            return;
+        }
+
+        GGVector initVel =
+                new GGVector(FORCE_CUESTICK_HIT * Math.cos(radians+Math.PI),
+                        FORCE_CUESTICK_HIT * Math.sin(radians+Math.PI));
+        cueBall.setterVel(initVel);
+    }
+
+    public void setEnabled(boolean enabled){this.enabled = enabled;}
+    public void setVisible(boolean visible){
+        this.visible = visible;
+        if (this.visible) {
+            show();
+        } else {
+            hide();
+        }
+    }
 }
