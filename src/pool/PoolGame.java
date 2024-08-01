6f62b5637c0e6e54f16b028b0b017e53622385df
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-08-01 20:13:02 +0200

    implemets gameframe to display status

--- src/pool/PoolGame.java
+++ src/pool/PoolGame.java
@@ -1,10 +1,33 @@
 package pool;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class PoolGame{
 
+    // TODO: refactor if time allows
     public static void main(String[] args) {
-        PoolTable poolTable = new PoolTable();
+        JFrame gameFrame = GameFrame.createGameFrame();
+        JLabel gameTimer = GameFrame.createGameTimerLabel();
+        JLabel ballCounter = GameFrame.createBallCounterLabel();
+
+        JPanel southPanel = new JPanel(new BorderLayout());
+        JPanel timerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel ballCounterPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        PoolTable poolTable = new PoolTable(gameTimer, ballCounter);
         poolTable.doRun();
-        poolTable.show();
+
+        timerPanel.add(gameTimer);
+        ballCounterPanel.add(ballCounter);
+
+        southPanel.add(timerPanel, BorderLayout.CENTER);
+        southPanel.add(ballCounterPanel, BorderLayout.WEST);
+
+        gameFrame.setLayout(new BorderLayout());
+        gameFrame.add(southPanel, BorderLayout.SOUTH);
+        gameFrame.add(poolTable, BorderLayout.CENTER);
+        gameFrame.setLocationRelativeTo(null);
+        gameFrame.setVisible(true);
     }
 }
