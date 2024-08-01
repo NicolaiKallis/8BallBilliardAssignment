8c077e1c770f16c0434d96c8c298d365f8d19f02
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-08-01 12:12:42 +0200

    implements parameter poolTable

--- src/pool/CueBall.java
+++ src/pool/CueBall.java
@@ -3,8 +3,8 @@ package pool;
 public class CueBall extends Ball {
     private static final String IMAGE_PATH = "assets/kugel_white.gif";
 
-    public CueBall() {
-        super(IMAGE_PATH, 0);
+    public CueBall(PoolTable poolTable) {
+        super(IMAGE_PATH, 0, poolTable);
     }
 
     @Override
