3f9f0384ade66a289083adf712f3fa0f5f08de61
NicolaiKallis <Nicolai.Kallis@gmx.de>
2024-08-01 03:54:19 +0200

    removes old code

--- src/pool/CueBall.java
+++ src/pool/CueBall.java
@@ -1,14 +1,9 @@
 package pool;
 
-import ch.aplu.jgamegrid.Actor;
-import ch.aplu.jgamegrid.Location;
-import ch.aplu.jgamegrid.GGVector;
-
 public class CueBall extends Ball {
     private static final String IMAGE_PATH = "assets/kugel_white.gif";
-    private static final int BALL_SIZE = 20;
 
-    public CueBall(int wallOffset) {
+    public CueBall() {
         super(IMAGE_PATH, 0);
     }
 
